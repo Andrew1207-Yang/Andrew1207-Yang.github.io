4380ed600fa7b7f2ed9637e89c82df5b297b093a
Andrew (Yao-an) Yang <a.yang353188@gmail.com>
2025-10-23 21:47:22 +0000

    force light theme

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -30,13 +30,16 @@ let setTheme = (theme) => {
     $("html").attr("data-theme") ||
     browserPref;
 
-  if (use_theme === "dark") {
-    $("html").attr("data-theme", "dark");
-    $("#theme-icon").removeClass("fa-sun").addClass("fa-moon");
-  } else if (use_theme === "light") {
-    $("html").removeAttr("data-theme");
-    $("#theme-icon").removeClass("fa-moon").addClass("fa-sun");
-  }
+  // if (use_theme === "dark") {
+  //   $("html").attr("data-theme", "dark");
+  //   $("#theme-icon").removeClass("fa-sun").addClass("fa-moon");
+  // } else if (use_theme === "light") {
+  //   $("html").removeAttr("data-theme");
+  //   $("#theme-icon").removeClass("fa-moon").addClass("fa-sun");
+  // }
+  // Force light theme
+  $("html").removeAttr("data-theme");
+  $("#theme-icon").removeClass("fa-moon").addClass("fa-sun");
 };
 
 // Toggle the theme manually
